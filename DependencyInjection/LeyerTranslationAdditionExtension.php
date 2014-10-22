f0c704fc936aa560e1c4eabbfe612789e9f5899f
Adrien Russo <arusso@idealtechnology.net>
2014-10-22 16:07:09 -0400

    remove unsed stuff

--- DependencyInjection/LeyerTranslationAdditionExtension.php
+++ DependencyInjection/LeyerTranslationAdditionExtension.php
@@ -2,7 +2,6 @@
 
 namespace Leyer\TranslationAdditionBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
